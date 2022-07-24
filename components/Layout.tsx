8bf88713e706593d9af02027ae74570b8afb5bf5
harddownloader <serafimdev@gmail.com>
2022-07-24 20:24:50 +0300

    add layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header';
+import Footer from "./Footer";
+
+export interface LayoutProps {
+  children?: React.ReactNode;
+}
+
+function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
