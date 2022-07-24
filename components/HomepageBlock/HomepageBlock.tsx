51b9d838f4f834e7317d516537ea7aec776f4cbe
harddownloader <serafimdev@gmail.com>
2022-07-25 01:06:56 +0300

    add home page components

---
+++ components/HomepageBlock/HomepageBlock.tsx
@@ -0,0 +1,14 @@
+// @flow
+import React from 'react';
+
+type Props = {
+
+};
+
+export const HomepageBlock = (props: Props) => {
+  return (
+    <div>
+      <span className="text-white">homeapgeBlock</span>
+    </div>
+  );
+};
