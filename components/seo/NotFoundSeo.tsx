5fe65d3107f21873faa4ed58d77f4203ad2cf414
harddownloader <serafimdev@gmail.com>
2022-07-25 00:33:31 +0300

    add seo components

---
+++ components/seo/NotFoundSeo.tsx
@@ -0,0 +1,28 @@
+import { NextSeo } from "next-seo";
+
+import { BLOG_NAME } from "@/lib/const";
+
+export function NotFoundSeo() {
+  const title = `Page Not found - ${BLOG_NAME}`;
+  const description = "Page not found.";
+
+  return (
+    <NextSeo
+      title={title}
+      description={description}
+      openGraph={{
+        title,
+        description,
+        images: [
+          {
+            url: "/logo.png",
+            alt: BLOG_NAME,
+          },
+        ],
+        site_name: BLOG_NAME,
+      }}
+    />
+  );
+}
+
+export default NotFoundSeo;
