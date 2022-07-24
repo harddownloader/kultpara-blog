5e8ecb04e7e4a1019453a1b69ad2e23d2aab11db
harddownloader <serafimdev@gmail.com>
2022-07-24 20:39:07 +0300

    add app config variables

---
+++ lib/const.ts
@@ -0,0 +1,4 @@
+export const BLOG_NAME = process.env.NEXT_PUBLIC_BLOG_NAME || "";
+export const API_URI = process.env.NEXT_PUBLIC_API_URI || "";
+export const HOMEPAGE_MENU = process.env.NEXT_PUBLIC_HOMEPAGE_MENU || "";
+export const GEOLOCATION = process.env.NEXT_PUBLIC_GEOLOCATION === "true";
