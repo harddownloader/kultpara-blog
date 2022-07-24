93dff416ad960eb735d56f32efdf95e1b1893354
harddownloader <serafimdev@gmail.com>
2022-07-24 19:45:32 +0300

    add _document

---
+++ pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, {DocumentContext, Html, Main, NextScript} from 'next/document';
+import Head from "next/head";
+
+class MyDocument extends Document<{ lang?:string }> {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps, lang: ctx?.query.locale };
+  }
+
+  render() {
+    // @ts-ignore
+    const uri: string = process.env.NEXT_PUBLIC_API_URI;
+    const { hostname } = new URL(uri);
+
+    return (
+      <Html lang={this.props.lang}>
+        <Head>
+          <link rel="preconnect" href={`//${hostname}`} crossOrigin="true" />
+          <link rel="dns-prefetch" href={`//${hostname}`} />
+        </Head>
+        <body>
+        <Main />
+        <NextScript/>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
