4ee0644ec155288909ae38066fdd8fd2b2e72ee7
harddownloader <serafimdev@gmail.com>
2022-07-28 21:39:40 +0300

    add apollo provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import '../styles/globals.scss'
 import React, { ReactElement, ReactNode } from "react";
 import { NextPage } from "next";
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ApolloProvider } from "@apollo/client";
+import apolloClient from "@/lib/graphql";
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
@@ -15,9 +17,9 @@ type NextPageWithLayout = NextPage & {
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page: ReactElement) => page);
   return (
-    <>
+    <ApolloProvider client={apolloClient}>
       {getLayout(<Component {...pageProps} />)}
-    </>
+    </ApolloProvider>
   );
 }
 
