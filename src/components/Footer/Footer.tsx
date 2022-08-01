7dff3660ac80880e871d40c5bd1f3d09556083d8
harddownloader <serafimdev@gmail.com>
2022-08-01 12:39:19 +0300

    update footer - add copyright

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BLOG_NAME } from '@/lib/const';
 
 export function Footer({}) {
+  const [year] = useState(new Date().getFullYear());
+
   return (
-    <div className={"container mx-auto px-10"}>
-      Footer
+    <div className={"container mx-auto px-10" +
+      "              bg-black\n" +
+      "              border-2\n" +
+      "              border-white" +
+      "              text-white\n" +
+      "              text-base\n" +
+      "              font-medium"}
+    >
+      <div className="w-full text-center">
+        <p className="p-2">
+          ©{ year } { BLOG_NAME }
+        </p>
+      </div>
     </div>
   );
 }
