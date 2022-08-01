347ec7dde2fe7a58451a6f143e50671ee170782e
harddownloader <serafimdev@gmail.com>
2022-08-01 19:37:08 +0300

    update _document - add lang from next-i18next for html lang attribute

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, {DocumentContext, Head, Html, Main, NextScript} from 'next/document';
+import { i18n } from '../../next-i18next.config';
 
 class MyDocument extends Document<{ lang?:string }> {
   static async getInitialProps(ctx: DocumentContext) {
@@ -7,19 +8,22 @@ class MyDocument extends Document<{ lang?:string }> {
   }
 
   render() {
+    const currentLocale =
+      this.props.__NEXT_DATA__.locale || i18n.defaultLocale
     // @ts-ignore
     const uri: string = process.env.NEXT_PUBLIC_API_URI;
     const { hostname } = new URL(uri);
 
     return (
-      <Html lang={this.props.lang}>
+      // <Html lang={this.props.lang}>
+      <Html lang={currentLocale}>
         <Head>
           <link rel="preconnect" href={`//${hostname}`} crossOrigin="true" />
           <link rel="dns-prefetch" href={`//${hostname}`} />
         </Head>
         <body>
-        <Main />
-        <NextScript/>
+          <Main />
+          <NextScript/>
         </body>
       </Html>
     );
