d01b281032d32c3b1e0a15549ac0ec9e80b402b5
harddownloader <serafimdev@gmail.com>
2022-08-01 12:25:32 +0300

    update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,24 +1,35 @@
 import Link from "next/link";
-import React from "react";
-
-import { NotFoundSeo } from "../components/seo/NotFoundSeo";
+import React, {ReactElement} from "react";
+import { NotFoundSeo } from "@/components/seo/NotFoundSeo";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import { Layout } from "@/components";
 
 
 function Custom404() {
+  const { t } = useTranslation('common');
 
   return (
     <>
       <NotFoundSeo />
-      <div className="min-h-screen bg-gray-100">
+      <div className="min-h-screen container mx-auto px-10">
 
         <div className="py-10">
           <header className="mb-4">
-            <div className="container px-8">Page not found</div>
+            <div className="container px-8 text-white">{ t('page_not_found') }</div>
           </header>
           <main>
             <div className="container px-8">
               <Link href={'/'} passHref>
-                <a href="pass">Go back home</a>
+                <a href="pass" className={"text-base px-3 py-2" +
+                  "              bg-black border-2 border-white text-white" +
+                  "              cursor-pointer" +
+                  "              hover:-translate-y-1\n" +
+                  "              transition\n" +
+                  "              duration-500\n" +
+                  "              ease-in-out"}>
+                  { t('go_back_home') }
+                </a>
               </Link>
             </div>
           </main>
@@ -27,5 +38,17 @@ function Custom404() {
     </>
   );
 }
+
+export const getStaticProps = async ({ locale }) => {
+  return {
+    props: {
+      ...await serverSideTranslations(locale),
+    }
+  };
+}
+
+Custom404.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
 
 export default Custom404;
