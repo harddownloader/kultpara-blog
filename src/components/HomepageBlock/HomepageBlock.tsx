d247ae6f53d77f7000513671dddbf18f278964d2
harddownloader <serafimdev@gmail.com>
2022-08-01 22:14:13 +0300

    update home page - move featured posts to bottom

--- src/components/HomepageBlock/HomepageBlock.tsx
+++ src/components/HomepageBlock/HomepageBlock.tsx
@@ -1,27 +1,20 @@
-// @flow
 import React from 'react';
-import { getPosts } from "../../services";
-import {FeaturedPosts} from "../FeaturedPosts";
-import {PostCard} from "../PostCard";
-import {PostWidget} from "../PostWidget";
-import {Categories} from "../Categories";
+import { FeaturedPosts } from "@/components/FeaturedPosts";
+import { PostWidget } from "@/components/PostWidget";
+import { Categories } from "@/components/Categories";
+import { PostCards } from "@/components/PostCards";
 
 type Props = {
   posts: any
 };
 
 
-
 export const HomepageBlock = ({ posts }: Props) => {
-  // console.log({posts})
   return (
     <div className="container mx-auto px-10 mb-8">
-      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => (
-            <PostCard key={index} post={post.node} />
-          ))}
+          <PostCards posts={posts} />
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
@@ -29,6 +22,9 @@ export const HomepageBlock = ({ posts }: Props) => {
             <Categories />
           </div>
         </div>
+      </div>
+      <div className="my-8 mt-12">
+        <FeaturedPosts />
       </div>
     </div>
   );
