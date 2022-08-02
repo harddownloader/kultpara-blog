d786ab8e01601f543d598d77e3fbb151f36d2ea7
harddownloader <serafimdev@gmail.com>
2022-08-02 11:55:11 +0300

    add i18 for categories

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 
 import { getCategories } from '@/services';
+import { useTranslation } from "next-i18next";
 
 export const Categories = () => {
   const [categories, setCategories] = useState([]);
+  const { t } = useTranslation('common');
 
   useEffect(() => {
     getCategories().then((newCategories) => {
@@ -14,7 +16,7 @@ export const Categories = () => {
 
   return (
     <div className="bg-black border-2 border-white shadow-lg p-8 pb-12 mb-8">
-      <h3 className="text-white text-xl mb-8 font-semibold border-b pb-4">Categories</h3>
+      <h3 className="text-white text-xl mb-8 font-semibold border-b pb-4">{t('categories')}</h3>
       {categories.map((category, index) => (
         <Link key={index} href={`/category/${category.slug}`}>
           <span className={`text-white cursor-pointer block ${(index === categories.length - 1) ? 'border-b-0' : 'border-b'} pb-3 mb-3`}>{category.name}</span>
