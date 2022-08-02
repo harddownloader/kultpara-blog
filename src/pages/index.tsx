316a22a531cd401a9abe50a9c979aa04dfc0d0a2
harddownloader <serafimdev@gmail.com>
2022-08-02 12:16:05 +0300

    home page index - add seo, i18, layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,52 @@
-export function HomeRedirection() {
-  return null;
+import { GetStaticPaths, GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import React, { ReactElement } from "react";
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import {
+  HomepageBlock,
+  Layout
+} from "@/components";
+import { BaseSeo } from "@/components/seo/BaseSeo";
+import { getPosts } from "@/services";
+
+
+function Home({ posts }) {
+  return (
+    <>
+      <BaseSeo />
+      <div className="py-10">
+        <header className="mb-4">
+          <div className="container" />
+        </header>
+        <main>
+          <HomepageBlock posts={posts} />
+        </main>
+      </div>
+    </>
+  );
 }
 
-export default HomeRedirection;
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  const { locale } = context;
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'comments', 'header', 'footer'])),
+      posts,
+    },
+    revalidate: 60 * 60, // value in seconds, how often ISR will trigger on the server
+  };
+};
+
+// export const getStaticPaths: GetStaticPaths = () => ({
+//   paths: [],
+//   fallback: "blocking",
+// });
+
+Home.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
+
+export default Home;
