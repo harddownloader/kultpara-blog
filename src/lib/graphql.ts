f64ca8211f2c8d47810b7224d64747b56888ba60
harddownloader <serafimdev@gmail.com>
2022-08-02 12:00:32 +0300

    add req for searching

--- src/lib/graphql.ts
+++ src/lib/graphql.ts
@@ -1,10 +1,7 @@
 import { ApolloClient, NormalizedCacheObject, createHttpLink } from "@apollo/client";
-// import { createFetch, createSaleorClient } from "@saleor/sdk";
 
 import { API_URI } from "./const";
 import { cache } from './cache';
-// import { DEFAULT_CHANNEL } from "./regions";
-// import { typePolicies } from "./typePolicies";
 
 const httpLink = createHttpLink({
   uri: API_URI,
@@ -13,15 +10,9 @@ const httpLink = createHttpLink({
 
 const apolloClient: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   link: httpLink,
-  // cache: new InMemoryCache({ typePolicies }),
   cache,
   ssrMode: typeof window === "undefined",
   connectToDevTools: true,
 });
-
-// export const saleorClient = createSaleorClient({
-//   apiUrl: API_URI,
-//   channel: DEFAULT_CHANNEL.slug,
-// });
 
 export default apolloClient;
