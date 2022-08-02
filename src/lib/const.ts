bc32e0ce6bc26f5d1aa140737f9349b57554502f
harddownloader <serafimdev@gmail.com>
2022-08-02 11:53:56 +0300

    update app vars

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -2,3 +2,8 @@ export const BLOG_NAME = process.env.NEXT_PUBLIC_BLOG_NAME || "";
 export const API_URI = process.env.NEXT_PUBLIC_API_URI || "";
 export const HOMEPAGE_MENU = process.env.NEXT_PUBLIC_HOMEPAGE_MENU || "";
 export const GEOLOCATION = process.env.NEXT_PUBLIC_GEOLOCATION === "true";
+export const BACKEND_ACCESS_TOKEN = process.env.BACKEND_ACCESS_TOKEN || "";
+export const MAIN_WEBSITE = process.env.NEXT_MAIN_WEBSITE_DOMAIN || "";
+export const FACEBOOK = "https://www.facebook.com/kultpara/";
+export const INSTAGRAM = "https://www.instagram.com/kultparavapeshop/";
+export const TELEGRAM = "https://t.me/kultpara";
