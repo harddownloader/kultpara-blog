d9d55bcefb15dbcb0d602a1048852141bbd629b7
harddownloader <serafimdev@gmail.com>
2022-08-02 19:38:56 +0300

    add locale for "blog"

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,36 +1,21 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Category } from "@/types/Category";
 import Link from 'next/link';
-import { BLOG_NAME } from "../../lib/const";
-import { getCategories } from '../../services';
-import { langsVar } from '../../lib/cache';
-import { useReactiveVar } from '@apollo/client';
-// import { ApolloClient } from 'apollo-client';
-// import { InMemoryCache } from 'apollo-cache-inmemory';
-//
-// const cache = new InMemoryCache();
-// const client = new ApolloClient({
-//   cache,
-//   resolvers: { /* ... */ },
-// });
-//
-// cache.writeData({
-//   data: {
-//     todos: [],
-//     visibilityFilter: 'SHOW_ALL',
-//     networkStatus: {
-//       __typename: 'NetworkStatus',
-//       isConnected: false,
-//     },
-//   },
-// });
-
+import { BLOG_NAME, MAIN_WEBSITE } from "@/lib/const";
+import { getCategories } from '@/services';
+import { useTranslation } from "next-i18next";
+import LanguageSwitcher from "@/components/LanguageSwitcher/LanguageSwitcher";
+import Socials from "@/components/Socials/Socials";
+import { MainMenu } from './MainMenu';
+import { Search } from './Search';
+import { socialsVar } from '@/lib/cache';
+import { useReactiveVar } from "@apollo/client";
+// import short_logo from './../../../public/short_logo.jpg';
 
 export function Header({}) {
-  const langs = useReactiveVar(langsVar);
-  console.log({ langsVar: langs });
+  const socials = useReactiveVar(socialsVar);
+  const { t } = useTranslation('common');
   const [categories, setCategories] = useState([]);
-  const [count, setCount] = useState(0);
 
   useEffect(() => {
     getCategories().then((newCategories) => {
@@ -40,45 +25,57 @@ export function Header({}) {
 
   return (
     <div className={`container mx-auto px-10 mb-8`}>
-      <div className="w-full py-2 px-8 bg-black border-2 border-white">
-        <div className="w-full inline-block py-2 bg-black">
-          <span className={"text-white"}>Lang: {langs}</span>
-          <button className={"bg-white h-4"} onClick={() => langsVar(langs !== 'UA' ? 'UA' : 'ENG')}>{langs}</button>
-          <button onClick={() => setCount(count + 1)}> { count }</button>
+      <div className="w-full py-2 px-2 bg-black border-2 border-white md:px-8">
+        <div className="w-full py-2 bg-black md:flex md:justify-between">
+          <div className={"flex justify-center"}>
+            {/*<Link*/}
+            {/*  href={MAIN_WEBSITE}*/}
+            {/*>*/}
+            {/*  <span className={"text-sm md:text-base px-3 py-2 bg-black border-2 border-white text-white cursor-pointer hover:-translate-y-1\n" +*/}
+            {/*    "              transition\n" +*/}
+            {/*    "              duration-500\n" +*/}
+            {/*    "              ease-in-out"}>*/}
+            {/*    {t('go_to_website')}*/}
+            {/*  </span>*/}
+            {/*</Link>*/}
+
+            <Search />
+          </div>
+          <div className="flex justify-center">
+            <Socials socials={socials} />
+          </div>
         </div>
 
-        <div className="w-full inline-block py-8 bg-black">
-          <div className="md:float-left block">
+        <div className="w-full flex justify-between py-8 bg-black">
+          <div className="flex justify-center align-baseline">
             <Link href={"/"}>
               <span className={"cursor-pointer font-bold text-4xl text-white"}>
                 { BLOG_NAME }
               </span>
             </Link>
-          </div>
-          <div className="hidden md:float-left md:contents">
-            {categories.map((category: Category) => {
-              return (
-                // (url?: { hash?: string }) => ({ pathname: '/[channel]/[locale]/[sitemap]' as const, query: { channel, locale, sitemap }, hash: url?.hash })
-                // <Link key={category.slug} href={`/en-US/category/${category.slug}`}>
-                <Link key={category.slug} href={`/en-US/category/${category.slug}`}>
 
-                  {/*// <Link key={category.slug} href={{*/}
-                  {/*//   pathname: `/[locale]/category/[slug]` as const,*/}
-                  {/*//   query: {*/}
-                  {/*//     locale: 'en-US',*/}
-                  {/*//     slug: category.slug*/}
-                  {/*//   }*/}
-                  {/*// }}>*/}
-                  <span className={'md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'}>
-                    {category.name}
-                  </span>
-                </Link>
-              );
-            })}
+            <Link href={"/"}>
+              <span className={"cursor-pointer font-thin text-4xl text-white capitalize"}>
+                { t('blog') }
+              </span>
+            </Link>
+            <Link href={"/"}>
+              <img
+                src="/short_logo.jpg"
+                alt={`Blog ${BLOG_NAME}`}
+                className={"h-10"}
+              />
+            </Link>
           </div>
+
+          <div className="flex justify-center  md:contents"></div>
+        </div>
+
+        <div className="w-full py-8 bg-black md:flex md:justify-between">
+          <MainMenu categories={categories} />
+          <LanguageSwitcher />
         </div>
       </div>
-
     </div>
   );
 }
