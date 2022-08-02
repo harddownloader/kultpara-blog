b9eb731f556f230291573f5c28673a1b9d32b2de
harddownloader <serafimdev@gmail.com>
2022-08-02 19:29:24 +0300

    add locale to subscribe

--- src/components/Subscribe/Subscribe.tsx
+++ src/components/Subscribe/Subscribe.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {useTranslation} from "next-i18next";
 
 export const Subscribe = () => {
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const { t } = useTranslation('common');
+
   return (
     <>
       <div className="flex justify-center">
@@ -11,9 +15,15 @@ export const Subscribe = () => {
           className={"p-2 bg-white text-base text-black"}
         />
         <button
+          onClick={() => {
+            if(!isSubscribed) setIsSubscribed(true);
+          }}
           className={"p-2 bg-black border-2 border-white text-base text-white"}
-        >Subscribe</button>
+        >{ t('subscribe') }</button>
       </div>
+      {isSubscribed && <div className="subscribed_msg m-1 text-center">
+        <p className={"text-base text-white"}>{ t('subscribe_success_text') }</p>
+      </div>}
     </>
   );
 };
