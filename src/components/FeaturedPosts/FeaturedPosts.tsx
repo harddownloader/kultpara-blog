a95eecab28ba698abf34adf46da983cfe8661021
harddownloader <serafimdev@gmail.com>
2022-08-02 12:08:47 +0300

    update slider with posts - set less visible cards in slider

--- src/components/FeaturedPosts/FeaturedPosts.tsx
+++ src/components/FeaturedPosts/FeaturedPosts.tsx
@@ -2,17 +2,17 @@ import React, { useState, useEffect } from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
-import { FeaturedPostCard } from '..';
-import { getFeaturedPosts } from '../../services';
+import { FeaturedPostCard } from '@/components/FeaturedPostCard';
+import { getFeaturedPosts } from '@/services';
 
 const responsive = {
   superLargeDesktop: {
     breakpoint: { max: 4000, min: 1024 },
-    items: 5,
+    items: 3,
   },
   desktop: {
     breakpoint: { max: 1024, min: 768 },
-    items: 3,
+    items: 2,
   },
   tablet: {
     breakpoint: { max: 768, min: 640 },
@@ -36,7 +36,7 @@ export const FeaturedPosts = () => {
   }, []);
 
   const customLeftArrow = (
-    <div className="absolute arrow-btn left-0 text-center py-3 cursor-pointer bg-pink-600 rounded-full">
+    <div className="absolute arrow-btn left-0 flex justify-center py-3 cursor-pointer bg-black border-2 border-white rounded-full">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
       </svg>
@@ -44,7 +44,7 @@ export const FeaturedPosts = () => {
   );
 
   const customRightArrow = (
-    <div className="absolute arrow-btn right-0 text-center py-3 cursor-pointer bg-pink-600 rounded-full">
+    <div className="absolute arrow-btn right-0 flex justify-center py-3 cursor-pointer bg-black border-2 border-white rounded-full">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
       </svg>
@@ -52,7 +52,7 @@ export const FeaturedPosts = () => {
   );
 
   return (
-    <div className="mb-8">
+    <div className="mb-8 block">
       <Carousel infinite customLeftArrow={customLeftArrow} customRightArrow={customRightArrow} responsive={responsive} itemClass="px-4">
         {dataLoaded && featuredPosts.map((post, index) => (
           <FeaturedPostCard key={index} post={post} />
