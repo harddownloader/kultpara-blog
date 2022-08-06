b35a5f94d128ddca0f4fcb90895e6b4c2c0a702b
harddownloader <serafimdev@gmail.com>
2022-08-06 16:30:52 +0300

    add images domain to next config (for next/image)

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const { i18n } = require('./next-i18next.config');
 module.exports = {
   reactStrictMode: true,
   i18n,
+  images: {
+    domains: ['media.graphassets.com'],
+  },
   // async redirects() {
   //   return [
   //     {
