d4c0d732babbb29ef9ba352e8059ddd6553742f6
harddownloader <serafimdev@gmail.com>
2022-08-07 19:38:19 +0300

    update home page view

--- src/components/HomepageBlock/HomepageBlock.tsx
+++ src/components/HomepageBlock/HomepageBlock.tsx
@@ -1,28 +1,13 @@
 import React from 'react';
 import { FeaturedPosts } from "@/components/FeaturedPosts";
-import { PostWidget } from "@/components/PostWidget";
-import { Categories } from "@/components/Categories";
-import { PostCards } from "@/components/PostCards";
-
-type Props = {
-  posts: any
-};
+import { NewPostsSection } from './NewPostsSection';
+import { Posts } from '@/types/Posts';
 
 
-export const HomepageBlock = ({ posts }: Props) => {
+export const HomepageBlock = ({ posts }: Posts) => {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="lg:col-span-8 col-span-1">
-          <PostCards posts={posts} />
-        </div>
-        <div className="lg:col-span-4 col-span-1">
-          <div className="lg:sticky relative top-8">
-            <PostWidget />
-            <Categories />
-          </div>
-        </div>
-      </div>
+      <NewPostsSection posts={posts}/>
       <div className="my-8 mt-12">
         <FeaturedPosts />
       </div>
