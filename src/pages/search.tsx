0cad56a6fe5cd87786f142502a43b13ad6a403bb
harddownloader <serafimdev@gmail.com>
2022-08-07 22:06:17 +0300

    search page - rm useless useless comments

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -3,7 +3,6 @@ import { getSearchResults } from "@/services";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { BaseSeo } from "@/components/seo/BaseSeo";
 import { SearchpageBlock, Layout } from "@/components";
-// import { GetStaticPropsContext } from "next";
 
 
 export const SearchPage = ({ posts }) => {
@@ -21,30 +20,15 @@ export const SearchPage = ({ posts }) => {
     );
 };
 
-// export async function getStaticProps(context: GetStaticPropsContext) {
-//     const { params , locale } = context
-//     console.log('getStaticProps', context )
-//     // const posts = (await getSearchResults(params.query)) || [];
-//     return {
-//         props: {
-//             posts: [],
-//             ...(await serverSideTranslations(locale, ['common', 'comments', 'header', 'footer'])),
-//         },
-//     };
-// }
-
 export async function getServerSideProps(context) {
     const { query , locale } = context
-    // console.log('getServerSideProps', context )
     const posts = (await getSearchResults(query.search)) || [];
-    // const posts = await getSearchResults(query.search);
-    console.log({posts})
 
     return {
         props: {
             posts: posts,
             ...(await serverSideTranslations(locale, ['common', 'comments', 'header', 'footer'])),
-        }, // will be passed to the page component as props
+        },
     }
 }
 
