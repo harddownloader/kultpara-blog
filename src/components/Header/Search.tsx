4def4a3533da18af971b40b8307416479f3bdfd6
harddownloader <serafimdev@gmail.com>
2022-08-07 21:46:14 +0300

    add search page block && add locale for search component and page

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import { useRouter } from "next/router";
+import { useTranslation } from "next-i18next";
 
 export const Search = () => {
     const router = useRouter();
     const [search, setSearch] = useState('');
+    const { t } = useTranslation('common');
 
   const searchHandler = () => {
       router.push({
@@ -19,7 +21,7 @@ export const Search = () => {
         <input
           type="text"
           name="search"
-          placeholder={"Search..."}
+          placeholder={`${t('search')}...`}
           className={"p-2 bg-white text-base text-black"}
           onChange={(e) => setSearch(e.target.value)}
         />
---
+++ src/components/SearchpageBlock/SearchpageBlock.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { PostCards } from "@/components/PostCards";
+import { Categories, PostWidget } from "@/components";
+import { FeaturedPosts } from "@/components/FeaturedPosts";
+import { Posts } from '@/types/Posts';
+import { useTranslation } from "next-i18next";
+
+
+export const SearchpageBlock = ({ posts }: Posts) => {
+    const { t } = useTranslation('common');
+
+    return (
+        <div className="container mx-auto px-10 mb-8">
+            <h1 className={"text-white text-4xl mb-4"}>{ t('search_results') }</h1>
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+
+                <div className="lg:col-span-8 col-span-1">
+                    { posts?.length
+                        ? <PostCards posts={posts} />
+                        : <p className={"text-white text-base"}>{ t('search_no_results') }</p>
+                    }
+                </div>
+                <div className="lg:col-span-4 col-span-1">
+                    <div className="lg:sticky relative top-8">
+                        <PostWidget />
+                        <Categories />
+                    </div>
+                </div>
+            </div>
+            <div className="my-8 mt-12">
+                <FeaturedPosts />
+            </div>
+        </div>
+    );
+};
+
+export default SearchpageBlock;
