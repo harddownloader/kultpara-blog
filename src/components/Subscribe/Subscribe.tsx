cd0dc554162f86b65cc5253df1954be2e3b556da
harddownloader <serafimdev@gmail.com>
2022-08-08 18:22:58 +0300

    update subscribe component - added sending mail

--- src/components/Subscribe/Subscribe.tsx
+++ src/components/Subscribe/Subscribe.tsx
@@ -1,27 +1,60 @@
 import React, { useState } from 'react';
-import {useTranslation} from "next-i18next";
+import { useTranslation } from "next-i18next";
+import { addSubscriber as addSubscriberReq } from "@/services";
+
 
 export const Subscribe = () => {
-  const [isSubscribed, setIsSubscribed] = useState(false);
+  const [subscribe, setSubscribe] = useState({
+    isSubscribed: false,
+    email: ''
+  });
   const { t } = useTranslation('common');
+
+  const addSubscriber = () => {
+    changeSubscribeStatusHandler(true);
+    sendEmailHandler()
+  }
+
+  const sendEmailHandler = () => {
+    if (subscribe?.email) {
+      const resAddSubscriber = addSubscriberReq(subscribe.email);
+      console.log({resAddSubscriber})
+    }
+  }
+
+  const subscriberHandler = (e: { target: { name: string; value: string; }; }) => {
+    setSubscribe({
+      ...subscribe,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  const changeSubscribeStatusHandler = (status: boolean) => {
+    setSubscribe({
+      ...subscribe,
+      isSubscribed: status
+    });
+  }
 
   return (
     <>
       <div className="flex justify-center">
         <input
           type="text"
-          name="subscribe"
+          name="email"
           placeholder={"Email"}
           className={"p-2 bg-white text-base text-black"}
+          value={subscribe.email}
+          onChange={subscriberHandler}
         />
         <button
           onClick={() => {
-            if(!isSubscribed) setIsSubscribed(true);
+            if(!subscribe.isSubscribed) addSubscriber();
           }}
           className={"p-2 bg-black border-2 border-white text-base text-white"}
         >{ t('subscribe') }</button>
       </div>
-      {isSubscribed && <div className="subscribed_msg m-1 text-center">
+      {subscribe.isSubscribed && <div className="subscribed_msg m-1 text-center">
         <p className={"text-base text-white"}>{ t('subscribe_success_text') }</p>
       </div>}
     </>
