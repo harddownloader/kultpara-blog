390688f8cfb1d28c9d2487ef9a600a1fea306fc5
harddownloader <serafimdev@gmail.com>
2022-08-10 01:45:44 +0300

    add types for language from useTranslation.i18n

--- src/components/AdjacentPosts/AdjacentPosts.tsx
+++ src/components/AdjacentPosts/AdjacentPosts.tsx
@@ -3,6 +3,7 @@ import { AdjacentPostCard } from '@/components';
 import { getAdjacentPosts } from '@/services';
 import { useTranslation } from "next-i18next";
 import { Post } from '@/types/Posts';
+import { LocaleEnum } from '@/types/Locale';
 
 
 export interface AdjacentPostType {
@@ -17,15 +18,16 @@ export interface AdjacentPostsProps {
 
 export const AdjacentPosts = ({ createdAt, slug }: AdjacentPostsProps) => {
   const { i18n: { language } } = useTranslation('common');
+  const langEnum: LocaleEnum = language as LocaleEnum;
   const [adjacentPost, setAdjacentPost] = useState<AdjacentPostType | null>(null);
   const [dataLoaded, setDataLoaded] = useState(false);
 
   useEffect(() => {
-    getAdjacentPosts(createdAt, slug, language).then((result) => {
+    if (slug && language) getAdjacentPosts(createdAt, slug, langEnum).then((result) => {
       setAdjacentPost(result);
       setDataLoaded(true);
     });
-  }, [slug]);
+  }, [slug, language]);
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-8 gap-12 mb-8">
--- src/types/Locale.ts
+++ src/types/Locale.ts
@@ -1,5 +1,5 @@
 export enum LocaleEnum {
     EN = 'en',
-    UA =  'ua',
-    RU =  'ru',
+    UA = 'ua',
+    RU = 'ru',
 }
