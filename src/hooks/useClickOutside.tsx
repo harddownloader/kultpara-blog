84c4e696b9d540ef1154c71b5fcb6e8e61372139
harddownloader <serafimdev@gmail.com>
2022-08-11 00:13:57 +0300

    fix ts error - add interface and types useClickOutside

--- src/hooks/useClickOutside.tsx
+++ src/hooks/useClickOutside.tsx
@@ -1,14 +1,20 @@
 import React, { useRef, useEffect } from "react";
 
+export interface UseOutside {
+  children: JSX.Element
+  setIsDropdown: () => void
+  isActive: boolean
+}
+
 /**
  * Hook that alerts clicks outside of the passed ref
  */
-function useOutsideAlerter(ref, setIsDropdown, isActive) {
+function useOutsideAlerter(ref: React.RefObject<HTMLElement>, setIsDropdown: () => void, isActive: boolean) {
   useEffect(() => {
     /**
      * Alert if clicked on outside of element
      */
-    function handleClickOutside(event) {
+    function handleClickOutside(event: { target: any; }) {
       if (ref.current && !ref.current.contains(event.target) && isActive) {
         // alert("You clicked outside of me!");
         setIsDropdown()
@@ -26,7 +32,7 @@ function useOutsideAlerter(ref, setIsDropdown, isActive) {
 /**
  * Component that alerts if you click outside of it
  */
-export function OutsideAlerter({ children, setIsDropdown, isActive }) {
+export function OutsideAlerter({ children, setIsDropdown, isActive }: UseOutside) {
   const wrapperRef = useRef(null);
   useOutsideAlerter(wrapperRef, setIsDropdown, isActive);
 
