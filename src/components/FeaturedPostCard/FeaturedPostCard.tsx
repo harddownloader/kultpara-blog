0fd4db56ff6f363b7baa99ac0297a88e5b886e0f
harddownloader <serafimdev@gmail.com>
2022-08-12 15:42:08 +0300

    fix react-multi-carousel error about carouselState

--- src/components/FeaturedPostCard/FeaturedPostCard.tsx
+++ src/components/FeaturedPostCard/FeaturedPostCard.tsx
@@ -11,14 +11,20 @@ export interface FeaturedPostCardProps {
 
 export const FeaturedPostCard = ({ post }: FeaturedPostCardProps) => (
   <div className="relative h-72">
-    <div className="absolute bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-72" style={{ backgroundImage: `url('${post.featuredImage.url}')` }} />
+    <Image
+      alt={post.slug}
+      layout={'fill'}
+      objectFit={'cover'}
+      objectPosition={'center'}
+      src={post.featuredImage.url}
+    />
     <div className="absolute bg-center bg-gradient-to-b opacity-50 from-black-400 via-black-700 to-black w-full h-72" />
     <div className="flex flex-col rounded-lg p-4 items-center justify-center absolute w-full h-full">
       <p className="text-white mb-4 text-shadow font-semibold text-xs">{moment(post.createdAt).format('MMM DD, YYYY')}</p>
       <p className="text-white mb-4 text-shadow font-semibold text-2xl text-center">{post.title}</p>
       <div className="flex items-center absolute bottom-5 w-full justify-center">
         <Image
-          unoptimized
+
           alt={post.author.name}
           height="30px"
           width="30px"
