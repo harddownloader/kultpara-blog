109b6696e15821ef16a917b3aedb6110c6f8ab70
harddownloader <serafimdev@gmail.com>
2022-08-14 00:37:23 +0300

    add localePath to next-18next config - text 1 of trying to fix production error with path for locales

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,11 @@
+const path = require('path');
+
 module.exports = {
     i18n: {
         defaultLocale: 'en',
         locales: ['en', 'ua', 'ru'],
+
     },
+    localePath: path.resolve('./public/locales'),
     reloadOnPrerender: process.env.NODE_ENV === 'development'
 };
