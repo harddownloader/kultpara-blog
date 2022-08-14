ec449b66f5ee805be122192c1cc5a5e3e6c7527f
harddownloader <serafimdev@gmail.com>
2022-08-14 17:01:32 +0300

    try to catch error v2

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -8,6 +8,6 @@ module.exports = {
     localePath: path.resolve('./public/static/locales'),
     // serializeConfig: false,
     // defaultNS: 'common',
-    debug: true, // process.env.NODE_ENV === 'development',
+    debug: process.env.NODE_ENV === 'development',
     reloadOnPrerender: process.env.NODE_ENV === 'development'
 };
